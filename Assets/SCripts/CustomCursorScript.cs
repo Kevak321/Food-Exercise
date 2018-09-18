baed1db74c998d886d4a8f965bfa90e67f8bbf90
Kevin Armstrong <kevak@hotmail.se>
2018-09-18 14:24:07 +0200

    Custom Cursor
    Added a custom cursor

---
+++ Assets/SCripts/CustomCursorScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomCursorScript : MonoBehaviour
+{
+
+    public Texture2D cursorTexture;
+    // Use this for initialization
+    void Start()
+    {
+        if (cursorTexture != null)
+        {
+            SetCustomCursor();
+        }
+    }
+
+    void SetCustomCursor()
+    {
+        Cursor.SetCursor(this.cursorTexture, Vector2.zero, CursorMode.Auto);
+    }
+}
